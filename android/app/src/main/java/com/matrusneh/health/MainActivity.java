ee8e4001b3a795e49f03f371ec4774cd3b568cf0
unknown <yashaswikb149@gmail.com>
2026-05-13 16:05:55 +0530

    Added Android app files

---
+++ android/app/src/main/java/com/matrusneh/health/MainActivity.java
@@ -0,0 +1,5 @@
+package com.matrusneh.health;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.matrusneh.health',
+  appName: 'MatruSneh Health',
+  webDir: 'dist'
+};
+
+export default config;
